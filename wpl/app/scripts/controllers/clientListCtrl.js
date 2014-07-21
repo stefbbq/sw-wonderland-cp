b8230ce86ee58e188e39e801dec0216811a1d8c0
Scott David Garson <sdgarson@gmail.com>
2014-07-21 16:11:25 -0400

    client search

--- wpl/app/scripts/controllers/clientListCtrl.js
+++ wpl/app/scripts/controllers/clientListCtrl.js
@@ -8,7 +8,17 @@
  * Controller of the wplAdminApp
  */
 angular.module('wplAdmin')
-  
+.controller('SearchCtrl', ['$scope', function($scope) {
+    
+    $scope.doSearch = function() {
+      if ($scope.searchString.length == 0) {
+        // nothing entered; show all
+        $scope.$emit('loadClientPage', 0);
+      } else {
+        $scope.$emit('searchClients', 0, $scope.searchString);
+      }
+    };
+}])
 .controller('ClientListCtrl', ['$scope', '$http', '$location', '$rootScope', 'clientListService', function ($scope, $http, $location, $rootScope, clientListService) {
     $scope.companies = [];
     $scope.clientListService = clientListService;
@@ -21,9 +31,22 @@ angular.module('wplAdmin')
       loadClientPage(startPage)
     });
     
+    $scope.$on('searchClients', function(e, startPage, searchString) {
+      searchClients(startPage, searchString)
+    });
+    
     function loadClientPage(startPage) {
-      $scope.clientListService.loadList(startPage);
+      $scope.clientListService.loadList(startPage, function(count) {
+        $scope.$broadcast('clientResultsLoaded', count);
+      });
     }
+    
+    function searchClients(startPage, searchString) {
+      $scope.clientListService.search(startPage, searchString, function(count) {
+        $scope.$broadcast('clientResultsLoaded', count);
+      });
+    }
+    
     
     $scope.onAddClient = function() {
       $location.path('/addClient');
@@ -33,33 +56,31 @@ angular.module('wplAdmin')
     
   }])
   
+  /*
+   * Pager
+   * This is the view which displays the pagination.
+   * It listens for the 'clientResultsLoaded' event 
+   * and draws based on the number of records available.
+   */
 .controller('Pager', function($scope, $http, $rootScope) {
-
-    var args = {action:'clientCount'};
     var pageList = [];
     var pageIndex = 0;
+    $scope.pages = [];
     
-    $http.jsonp($rootScope.wsURL, 
-    {
-        params:args,
-        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
-    })
-    .success(function(result) {
-      var recordCount = result.data.total;
+    $scope.$on('clientResultsLoaded', function(e, recordCount) {
       var pageSize = $rootScope.clientList.pageSize;
       var pageCount = Math.ceil(recordCount / pageSize);
       var recordNum = 0;
+      pageList.length = 0;
       for (var i=0; i<pageCount; i++) {
         pageList.push({num:i + 1, recordNum:recordNum})
         recordNum += pageSize;
       }
       
-      $scope.pages = pageList;
-    })          
-    .error (function(result) {
-        console.log('error', result);
+      angular.copy(pageList, $scope.pages);
     });
     
+  
     $scope.prevPage = function() {
       if (pageIndex > 0) {
         $scope.loadPage(pageIndex - 1);
@@ -130,7 +151,7 @@ angular.module('wplAdmin')
       var list = [];
       var pageSize = $rootScope.clientList.pageSize;
       
-      function loadList(startPage) {
+      function loadList(startPage, callback) {
         var args = {action:'clientList', s:startPage, c:pageSize};
         
         $http.jsonp($rootScope.wsURL, 
@@ -139,19 +160,37 @@ angular.module('wplAdmin')
             headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
         })
         .success(function(result) {
-          angular.copy(result.data, list);
-          //console.log('success', list);
+          angular.copy(result.data.list, list);
+          var count = result.data.count;
+          callback(count);
         })          
         .error (function(result) {
             console.log('error', result);
         });        
       }
 
-
+      function searchList(startPage, searchString, callback) {
+        var args = {action:'clientSearch', s:startPage, c:pageSize, q:searchString};
+        
+        $http.jsonp($rootScope.wsURL, 
+        {
+            params:args,
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+        })
+        .success(function(result) {
+          angular.copy(result.data.list, list);
+          var count = result.data.count;
+          callback(count);
+        })          
+        .error (function(result) {
+            console.log('error', result);
+        });         
+      }
 
       return {
         list:list,
-        loadList:loadList
+        loadList:loadList,
+        search:searchList
       };
 
       
